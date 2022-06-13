8a37386ae9f1cdeb112cf76696e1fb7f53c81636
vincenbryaniel@gmail.com <vincenbryaniel@gmail.com>
2022-06-13 22:40:45 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+// setting up the event listener to detect when the HTML page has loaded and the document is ready to be manipulated
+  document.addEventListener( "DOMContentLoaded", function () {
+    updateDOM()
+  } );
+  
+  //modifying the DOM element content
+function updateDOM() {
+    document.getElementById( "text" )
+     .textContent = "This is really cool!";
+  }
+
+
+
+
